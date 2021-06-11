c9c1eb35725dc57d429c02f744b5678c5b951502
Alex Kio <alexandermartelkio@gmail.com>
2021-06-11 10:40:54 -0400

    Create min max control for date picker

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,11 @@
 import './sass/index.scss';
 // import domUpdates from './domUpdates';
 // import { fetchApiData, postApiData } from './apiCalls';
+// const dayjs = require('dayjs')
+
+var today = new Date();
+var todayFormatted = today.getFullYear()+'-'+('0' + (today.getMonth()+1)).slice(-2)+'-'+('0' + today.getDate()).slice(-2);
+var inAYearFormatted = (today.getFullYear()+1)+'-'+('0' + (today.getMonth()+1)).slice(-2)+'-'+('0' + today.getDate()).slice(-2);
+var dateControl = document.querySelector('input[type="date"]');
+dateControl.min = todayFormatted;
+dateControl.max = inAYearFormatted;
